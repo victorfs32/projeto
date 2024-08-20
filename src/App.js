25352550e2baf00e499d511ce6e8c8faa643cf52
victorfs32 <jvfds32@gmail.com>
2024-08-20 10:24:40 -0300

    Ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"; // CSS principal
+import logo from "./img/LOGO.png";
+import abcImg from "./img/ABC.png";
+import saudacoesImg from "./img/SAUDAÇOES.png";
+import numeroImg from "./img/NUMERO.png";
 
 function App() {
   return (
     <div className="App">
+      {/* Logo */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="logo" alt="logo" />
       </header>
+
+      {/* Atividades */}
+      <main className="App-main">
+        <h3>ATIVIDADES</h3>
+        <div className="activities">
+          <a
+            href="https://kahoot.it/solo/?quizId=fdfca022-a08f-4e93-8676-734981889aaa"
+            className="activity-link"
+          >
+            <img
+              src={abcImg}
+              className="activity-img"
+              alt="Alfabeto em LIBRAS"
+            />
+            <h4>Alfabeto em LIBRAS</h4>
+          </a>
+          <a
+            href="https://kahoot.it/solo/?quizId=1d372228-82e2-476b-871a-019ae28a39fd"
+            className="activity-link"
+          >
+            <img
+              src={saudacoesImg}
+              className="activity-img"
+              alt="Saudações em LIBRAS"
+            />
+            <h4>Saudações em LIBRAS</h4>
+          </a>
+          <a
+            href="https://kahoot.it/solo/?quizId=fdfca022-a08f-4e93-8676-734981889aaa"
+            className="activity-link"
+          >
+            <img
+              src={numeroImg}
+              className="activity-img"
+              alt="Números em LIBRAS"
+            />
+            <h4>Números em LIBRAS</h4>
+          </a>
+        </div>
+      </main>
+
+      {/* Rodapé */}
+      <footer className="App-footer">
+        <p>&copy; 2024 Ensinando Libras. Todos os direitos reservados.</p>
+        <p>
+          <a href="https://www.seusite.com" className="footer-link">
+            Visite nosso site
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
