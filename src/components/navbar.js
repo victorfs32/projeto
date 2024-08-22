c31b93b735b675edc8ea0ccc8ad206d5040e8afd
victorfs32 <jvfds32@gmail.com>
2024-08-22 16:04:45 -0300

    Upar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './navbar.css';
 
-const navbar = () => {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <header className="header">
+      <a href='/' className="logo">Logo</a>
+
+      <button className="menu-toggle" onClick={toggleMenu}>
+        &#9776;
+      </button>
+
+      <nav className={`navbar ${isOpen ? 'open' : ''}`}>
+        <a href='/' className="nav-link">Home</a>
+        <a href='/atividades' className="nav-link">Atividades</a>
+        <a href='/sobre' className="nav-link">Sobre</a>
+      </nav>
+    </header>
+  );
 }
 
-export default navbar
+export default Navbar;
