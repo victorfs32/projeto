20f4627847f8ac1462d47a8dcf9a215dcb6c5792
victorfs32 <jvfds32@gmail.com>
2024-08-22 16:14:30 -0300

    Colocado a Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
+import logo from "../img/LOGO.png";
 
 const Navbar = () => {
   return (
     <header className="header">
-      <a href='/' className="logo">Logo</a>
+      <a href="/" className="logo">
+        <img
+          src={logo}
+          alt="Logo"
+        />
+      </a>
 
       <nav className="navbar">
-        <a href='/' className="nav-link">Home</a>
-        <a href='/atividades' className="nav-link">Atividades</a>
-        <a href='/sobre' className="nav-link">Sobre</a>
+        <a href="/" className="nav-link">
+          Home
+        </a>
+        <a href="/atividades" className="nav-link">
+          Atividades
+        </a>
+        <a href="/sobre" className="nav-link">
+          Sobre
+        </a>
       </nav>
     </header>
   );
-}
+};
 
 export default Navbar;
