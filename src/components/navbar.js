daff729a12ace6e416317d1f153042b38c2c938d
victorfs32 <jvfds32@gmail.com>
2024-08-22 16:07:38 -0300

    Alteração

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './navbar.css';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <header className="header">
       <a href='/' className="logo">Logo</a>
 
-      <button className="menu-toggle" onClick={toggleMenu}>
-        &#9776;
-      </button>
-
-      <nav className={`navbar ${isOpen ? 'open' : ''}`}>
+      <nav className="navbar">
         <a href='/' className="nav-link">Home</a>
         <a href='/atividades' className="nav-link">Atividades</a>
         <a href='/sobre' className="nav-link">Sobre</a>
