be4f12a005f59875dab40df08a317ff8ae668ec1
victorfs32 <jvfds32@gmail.com>
2024-08-23 10:06:48 -0300

    a

--- src/pages/Quiz/Alfabeto.js
+++ src/pages/Quiz/Alfabeto.js
@@ -152,7 +152,7 @@ function Quiz({ userName }) {
       } else {
         setShowScore(true);
       }
-    }, 3000); // Espera 1 segundo antes de passar para a próxima pergunta
+    }, 2000); // Espera 1 segundo antes de passar para a próxima pergunta
   };
 
   return (
