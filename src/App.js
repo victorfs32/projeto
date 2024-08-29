212a14ae724e1bc3d3611130ae6139d5a3ad74cc
victorfs32 <jvfds32@gmail.com>
2024-08-29 09:23:06 -0300

    ATUALIZADO

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-// src/App.js
-import React from 'react';
-import './App.css';
-import TestConnection from './TestConnection';
+import React from "react";
+import "./App.css"; // CSS principal
+import logo from "./img/LOGO.png";
+import Navbar from "./components/navbar"; // Nome do componente renomeado
+import Footer from "../src/components/footer"; // Importa o componente Footer
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
-    <div className="App">
-      <TestConnection />
-    </div>
+    <>
+      <Navbar /> {/* Uso do componente Navbar com letra maiúscula */}
+      <div className="App">
+        {/* Logo */}
+        <header className="App-header">
+          <img src={logo} className="logo" alt="logo" />
+        </header>
+        {/* Uso do componente Footer */}
+        <Footer />
+      </div>
+    </>
   );
 }
 
