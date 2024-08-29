cb2a7619fd26aba08652018df03cac7311302923
victorfs32 <jvfds32@gmail.com>
2024-08-29 09:01:48 -0300

    ATUALIZADO

--- src/TestConnection.js
+++ src/TestConnection.js
@@ -1,17 +1,23 @@
 // src/TestConnection.js
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const TestConnection = () => {
-  const [response, setResponse] = useState('');
+  const [response, setResponse] = useState("");
 
   const testBackend = () => {
-    fetch('https://ensinandolibras.com.br')
-      .then(response => response.text())
-      .then(data => {
+    fetch("https://www.ensinandolibras.com.br/") // Ajuste se necessário
+      .then((response) => {
+        // Verificar se a resposta é OK antes de processar
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then((data) => {
         setResponse(`Resposta do Backend: ${data}`);
       })
-      .catch(error => {
-        setResponse(`Erro: ${error}`);
+      .catch((error) => {
+        setResponse(`Erro: ${error.message}`);
       });
   };
 
