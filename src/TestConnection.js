aa7b4e39266a7f2a4951b92301764556ecfe44f9
victorfs32 <jvfds32@gmail.com>
2024-08-29 09:15:38 -0300

    ATUALIZADO

--- src/TestConnection.js
+++ src/TestConnection.js
@@ -1,18 +1,11 @@
-// src/TestConnection.js
 import React, { useState } from "react";
 
 const TestConnection = () => {
   const [response, setResponse] = useState("");
 
   const testBackend = () => {
-    fetch("https://www.ensinandolibras.com.br/") // Ajuste se necessário
-      .then((response) => {
-        // Verificar se a resposta é OK antes de processar
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.text();
-      })
+    fetch("https://backend-eosin-chi-12.vercel.app/") // URL do backend
+      .then((response) => response.text())
       .then((data) => {
         setResponse(`Resposta do Backend: ${data}`);
       })
